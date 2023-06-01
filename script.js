05fc80fe431a48b74277af52b87a889dfff0d9dd
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-06-01 15:16:21 -0400

    get id of cell and push into array functionality

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let container = document.querySelector('.container');
 
 const gameBoard = (() => {
     let boardArray = [
-        ['1','2','3'],
-        ['4', '5','6'],
-        ['7','8','9']
+        [1,2,3],
+        [4, 5,6],
+        [7,8,9]
     ]
     for(i=0; i< boardArray.length; i++){
         let board = boardArray[i];
@@ -12,9 +12,7 @@ const gameBoard = (() => {
             let block = document.createElement('div')
             block.classList.add('block')
             container.appendChild(block)
-            block.addEventListener('click', ()=>{
-                
-            })
+            block.setAttribute('id', 'cell ' + board[_])
             }
     }
 })();
@@ -32,20 +30,20 @@ const Game = () =>{
 
     blocks.forEach((block) =>{
         block.addEventListener('click', (event)=>{
-            handleCellClick = (() =>{
+            function handleCellClick(){
+                // add array for already selected cells, when a cell is clicked, push it to the array, and if it's noticed 
+                //someone already clicked a selected cell, stop them somehow.
+                let currentCell;
                 let selectedCells = [];
 
-                const clickedCell = event.target;
-                selectedCells.push(clickedCell);
-                console.log(selectedCells)
-
-                if(selectedCells.includes(clickedCell)){
-                    player1Turn = true;
-                }else{
-                    player1Turn = false
+                const clickedCell = event.target.id;
+                selectedCells.push(clickedCell)
+                console.log(clickedCell, selectedCells)
+                if(selectedCells.includes(currentCell)){
+                    console.log('no')
                 }
-
-            })();
+                // finding out the exact location of an array to would help with the logic for winning the game.
+            }
             
             if(player1Turn){
                 block.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>'
@@ -56,6 +54,7 @@ const Game = () =>{
                 player1Turn = true;
                 player2Turn = false;
             }
+            handleCellClick()
         })
     })
 
